65b10e065138369ed8bf831406b489ed71dbe2fd
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2021-06-25 14:42:36 +0100

    Localize with expo localize

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import * as Localization from 'expo-localization';
+import i18n from 'i18n-js';
+import { zh, en, es } from './i18n/supportedLanguages';
+
+i18n.fallbacks = true;
+i18n.translations = { en, zh, es };
+i18n.locale = Localization.locale;
 
 export default function App() {
+  const onPressLogin = () => {
+    Alert.alert(`${i18n.t('signoutBtn')}`, `${i18n.t('signOutAlertMess')}`, [
+      {
+        text: `${i18n.t('signOutAlertTitle')}`,
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      {
+        text: `${i18n.t('confirm')}`,
+        onPress: () => console.log('OK Pressed'),
+      },
+    ]);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{i18n.t('welcome')}</Text>
+      <Button
+        onPress={onPressLogin}
+        title={i18n.t('signoutBtn')}
+        color="#005792"
+        accessibilityLabel={i18n.t('signoutBtn')}
+      />
       <StatusBar style="auto" />
     </View>
   );
---
+++ i18n/supportedLanguages.js
@@ -0,0 +1,29 @@
+const en = {
+  welcome: 'Internationalization and Localization in React Native',
+  signoutBtn: 'Sign out',
+  signOutAlertTitle: 'Cancel',
+  signOutAlertMess: 'Are you sure you want to sign out?',
+  confirm: 'Okay',
+  resetBtn: 'Reset password',
+};
+
+const zh = {
+  welcome: 'React Native 中的国际化和本地化',
+  signoutBtn: '登出',
+  signOutAlertTitle: '取消',
+  signOutAlertMess: '您确定要退出吗？',
+  confirm: '好的',
+  resetBtn: '重设密码',
+};
+
+export const es = {
+  welcome: 'Internacionalización y localización en React Native',
+  signoutBtn: 'desconectar',
+  signOutAlertTitle: 'Cancelar',
+  signOutAlertMess: '¿Estás segura de que quieres cerrar sesión?',
+  confirm: 'Okey',
+
+  resetBtn: 'Restablecer la contraseña',
+};
+
+export { zh, en };
